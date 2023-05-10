c62b27e1239dd81bf0f0620439d3a0f83fd37e24
simon-swe <64075997+simon-swe@users.noreply.github.com>
2023-05-10 16:49:18 +0200

    Add files via upload
    Nyaste. Se kommentarer i koden för vilka funktioner som fungerar.

--- buss_sim.py
+++ buss_sim.py
@@ -9,19 +9,20 @@ RADIUS = 300
 NUM_BUSSES = 10
 BUS_RADIUS = 5
 MAX_SPEED = 0.005
-LAMBDA_POSSION = 2
+LAMBDA_POSSION = 1
 NUM_BUSSTOPS = 10
-boardingPerTime = 30
+boardingPerTime = 10
 timeBetweenStop = 2*math.pi / NUM_BUSSTOPS / (MAX_SPEED+MAX_SPEED/10)/2
 timeOnStop = timeBetweenStop/LAMBDA_POSSION/boardingPerTime
-waitTime = 30
+waitTime = 0
+busAdded = False
 
 #Olika lösningar, inte helt implementerat
-modeWait = False
-modeWaitSched = False
-modeSpeed = False
-modeDriveOver = False
-modeExtraBus = False
+modeWait = False #Fungerar: Väntar lite på varje hållplats
+modeWaitSched = True #Fungerar: Om den åkt längre än sin beräknade snitt vid en hållplats väntar den tills snittet har hunnit ifatt
+modeSpeed = False #Fungerar ej
+modeDriveOver = False #Fungerar ej
+modeExtraBus = False #Fungerar men tror att mätmetoden som utvärderar lösningen inte fungerar då. quick fix
 # Initiera pygame
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -33,7 +34,7 @@ class Bus:
     def __init__(self, angle): #Klassattributer
         self.angle = angle
         self.speed = 0
-        self.stop_time = 0
+        self.stop_time = 0.0
         self.stopped = False
         self.stopPassed = 0
         self.disTraveled = 0
@@ -44,27 +45,32 @@ class Bus:
             self.disTraveled += self.speed
             if self.angle > 2 * math.pi:
                 self.angle -= 2 * math.pi
+            self.stop_time = 0
 
     def check_for_stop(self): #Kollar om man är på ett busstop, och lägger till hur länge bussen ska stanna
             for stop in busStops:
                 con = stop.angle - self.angle
                 if ((con >= 0) and (con <= self.speed)) or ((con + 2 * math.pi >= 0) and (con + 2 * math.pi <= self.speed)):
                     self.angle = stop.angle
-                    self.stop_time += stop.people/boardingPerTime #Lägger till hur läänge bussen ska stanna
-                    stop.removePeople(1) #Tar bort folk från busshållsplatsen
                     if modeWait and self.stopped == False:
                         self.stop_time += waitTime
+                    if stop.people>boardingPerTime and self.stop_time<=0:
+                        self.stop_time += stop.people/boardingPerTime
+                        stop.people=0
+                    print("\n",stop.people)
+                    stop.removePeople() #Tar bort folk från busshållsplatsen
+                    print(stop.people,"\n")
+                    
                     self.stopped = True
+                    
 
     def update_stop_time(self, time): #Updaterar stoptiden, samt lite krimskrams
         if self.stopped:
             self.stop_time -= 1
-            print("Time: ",time)
-            print("Expected time: ",(timeBetweenStop + timeOnStop + waitTime)*(self.stopPassed+1), "\n") #Dumt att använda tid, använd distansen under om något
             print("Distance tracaled: ",self.disTraveled)
-            print("Expected dist: ", (MAX_SPEED+MAX_SPEED/10)/2*time*0.97)
+            print("Expected dist: ", (MAX_SPEED+MAX_SPEED/10)/2*time*timeBetweenStop/(timeBetweenStop+timeOnStop+waitTime),"\n")
             if modeWaitSched:
-                if time >= (timeBetweenStop + timeOnStop + waitTime)*(self.stopPassed+1):
+                if self.disTraveled < (MAX_SPEED+MAX_SPEED/10)/2*time*timeBetweenStop/(timeBetweenStop+timeOnStop+waitTime):
                     if self.stop_time <= 0:
                         self.stopped = False
                         self.stop_time = 0
@@ -74,7 +80,6 @@ class Bus:
             else:
                 if self.stop_time <= 0:
                     self.stopped = False
-                    self.stop_time = 0
                     self.stopPassed += 1
 
     def draw(self, surface):
@@ -91,8 +96,9 @@ class BusStop: #Hållplats klass
         amount = random.expovariate(LAMBDA_POSSION)
         self.people+=amount
 
-    def removePeople(self,time):
-        self.people -= min(self.people*time, boardingPerTime*time)
+    def removePeople(self):
+        self.people -= min(self.people, boardingPerTime)
+        
 
 def bunchingScore(buses): #Metod för att betygsätta lösningen för att motverka bunching. Kanske borde lägga till metod för att mäta flöde av människor
     distances = 0
@@ -101,10 +107,10 @@ def bunchingScore(buses): #Metod för att betygsätta lösningen för att motver
         for bus2 in buses:
             if bus1==bus2:
                 continue
-        if abs(bus1.angle-bus2.angle)<closest:
-            closest=bus1.angle-bus2.angle
-        distances += closest
-    score = abs(distances - 2*math.pi) #Kan behöva förklaras. Mindre är bättre
+            if abs(bus1.angle-bus2.angle)<closest:
+                closest=abs(bus1.angle-bus2.angle)
+        distances += abs(closest-2*math.pi/NUM_BUSSES)
+    score = abs(distances) #Kan behöva förklaras. Mindre är bättre
     return score
 
 # Skapa bussar
@@ -148,9 +154,18 @@ while running:
         bus.draw(screen)
     pygame.display.flip()
 
-    clock.tick(300)
-    if time==5000:
-        print(bunchingScore(buses))
+    
+    if modeExtraBus and busAdded == False and bunchingScore(buses)>2: #Lägger till extrabuss
+        buses.append(Bus(0.01)) 
+        NUM_BUSSES += 1
+        print("Bus Added")
+        busAdded = True
+
+    if time==5000: #Bestämmer hur länge programmet kör (iterationer)
+        print("Score: ",bunchingScore(buses))
+        print(busAdded) #Om man har lagt till 
         break
+
+    clock.tick(600)
 
 pygame.quit()
