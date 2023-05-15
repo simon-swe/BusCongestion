e970266365dba7afc6cc1adb03805e9259ffcdb0
simon-swe <64075997+simon-swe@users.noreply.github.com>
2023-05-15 13:14:38 +0200

    Add files via upload

--- buss_sim.py
+++ buss_sim.py
@@ -3,28 +3,25 @@ import math
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
 # Konstanter
 WIDTH, HEIGHT = 800, 800
 CENTER = (WIDTH // 2, HEIGHT // 2)
 RADIUS = 300
-NUM_BUSSES = 10
+NUM_BUSSES = 8
 BUS_RADIUS = 5
 MAX_SPEED = 0.005
-LAMBDA_POSSION = 1
-NUM_BUSSTOPS = 10
-boardingPerTime = 10
+LAMBDA_POSSION = 0.5
+NUM_BUSSTOPS = 8
+boardingPerTime = 20
 timeBetweenStop = 2*math.pi / NUM_BUSSTOPS / (MAX_SPEED+MAX_SPEED/10)/2
 timeOnStop = timeBetweenStop/LAMBDA_POSSION/boardingPerTime
 waitTime = 3
 busAdded = False
 
 #Olika lösningar, inte helt implementerat
-modeWait = True #Fungerar: Väntar lite på varje hållplats
-modeWaitSched = False #Fungerar: Om den åkt längre än sin beräknade snitt vid en hållplats väntar den tills snittet har hunnit ifatt
-modeSpeed = False #Fungerar ej
-modeDriveOver = False #Fungerar ej
-modeExtraBus = False #Fungerar men tror att mätmetoden som utvärderar lösningen inte fungerar då. quick fix
+modeWait = False #Fungerar: Väntar lite på varje hållplats
+modeWaitSched = False #Fungerar: Väntar in sin beräknade avgång
+modeExtraBus = False #Fungerar: Lägger till en till buss
 # Initiera pygame
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -33,7 +30,7 @@ clock = pygame.time.Clock()
 
 # Buss klass
 class Bus:
-    def __init__(self, angle): #Klassattributer
+    def __init__(self, angle): #Klassattribute r
         self.angle = angle
         self.speed = 0
         self.stop_time = 0.0
@@ -49,25 +46,24 @@ class Bus:
                 self.angle -= 2 * math.pi
             self.stop_time = 0
 
-    def check_for_stop(self): #Kollar om man är på ett busstop, och lägger till hur länge bussen ska stanna
+    def check_for_stop(self): #Kollar om och hur länge bussen ska stanna
             for stop in busStops:
                 con = stop.angle - self.angle
-                if ((con >= 0) and (con <= self.speed)) or ((con + 2 * math.pi >= 0) and (con + 2 * math.pi <= self.speed)):
+                if ((con >= 0) and (con <= self.speed)) or ((con + 2 * math.pi >= 0) and
+                                                             (con + 2 * math.pi <= self.speed)):
                     self.angle = stop.angle
-                    if modeWait and self.stopped == False:
-                        self.stop_time += waitTime
-                    if stop.people>boardingPerTime and self.stop_time<=0:
+                    if self.stopped == False:
+                        self.stop_time += abs(waitTime - stop.people/boardingPerTime)
+                    else:
                         self.stop_time += stop.people/boardingPerTime
-                        stop.people=0
+                    stop.people = 0
                     print("\n",stop.people)
                     self.stopped = True
                     
 
-    def update_stop_time(self, time): #Updaterar stoptiden, samt lite krimskrams
+    def update_stop_time(self, time): #Updaterar stoptiden
         if self.stopped:
             self.stop_time -= 1
-            print("Distance tracaled: ",self.disTraveled)
-            print("Expected dist: ", (MAX_SPEED+MAX_SPEED/10)/2*time*timeBetweenStop/(timeBetweenStop+timeOnStop+waitTime),"\n")
             if modeWaitSched:
                 if self.disTraveled < (MAX_SPEED+MAX_SPEED/10)/2*time*timeBetweenStop/(timeBetweenStop+timeOnStop+waitTime):
                     if self.stop_time <= 0:
@@ -99,7 +95,7 @@ class BusStop: #Hållplats klass
         self.people -= min(self.people, boardingPerTime)
         
 
-def bunchingScore(buses): #Metod för att betygsätta lösningen för att motverka bunching. Kanske borde lägga till metod för att mäta flöde av människor
+def bunchingScore(buses): #Betygsätter lösningen
     distances = 0
     for bus1 in buses:
         closest = 2 * math.pi
@@ -113,7 +109,11 @@ def bunchingScore(buses): #Metod för att betygsätta lösningen för att motver
     return score
 
 # Skapa bussar
-buses = [Bus(2 * math.pi * i / NUM_BUSSES + 0.01) for i in range(NUM_BUSSES)]
+if modeExtraBus:
+    ++NUM_BUSSES
+    buses = [Bus(2 * math.pi * i / NUM_BUSSES + 0.01) for i in range(NUM_BUSSES)]
+else:
+    buses = [Bus(2 * math.pi * i / NUM_BUSSES + 0.01) for i in range(NUM_BUSSES)]
 # Skapar hållplatser
 busStops = [BusStop(2*math.pi * i / NUM_BUSSTOPS) for i in range(NUM_BUSSTOPS)]
 
@@ -156,26 +156,18 @@ while running:
         bus.draw(screen)
     pygame.display.flip()
 
-    
-    if modeExtraBus and busAdded == False and bunchingScore(buses)>2: #Lägger till extrabuss
-        buses.append(Bus(0.01)) 
-        NUM_BUSSES += 1
-        print("Bus Added")
-        busAdded = True
-
-    if time==5000: #Bestämmer hur länge programmet kör (iterationer)
+    if time==15000: #Bestämmer hur länge programmet kör (iterationer)
         print(busAdded) #Om man har lagt till 
         break
     
     if time%100==0:
         bunchingScores.append((bunchingScore(buses), time))
-    clock.tick(600)
+    clock.tick(200)
 
 for i in bunchingScores:
     print("Score: ", i[0], " Vid tid: ", i[1])
 print("Tillagd buss: ", busAdded)
 pygame.quit()
-
 
 # Dela i två listor
 deviation = [x[0] for x in bunchingScores]
